235d9eb9d7228ab469bc598e4dd0c1912ee040e5
Julian Vitenson <jviten@ad.unc.edu>
2025-10-11 17:46:02 -0400

    Home page and png updates

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -3,20 +3,20 @@
 const HomePage = () => {
   return (
     <>
-        <div>
-            <header>
-                <h1>My Simple React Home Page</h1>
-            </header>
-            <main>
-                <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
-            </main>
-             <button>
-                Nomral Button
-             </button>
-             <p>
-                Edit <code>src/App.jsx</code> and save to test HMR
-             </p>
-        </div>
+      <div>
+        <header>
+          <h1>Welcome to Food Finder</h1>
+        </header>
+        <main>
+          <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
+        </main>
+        <button>
+          Click to Start!
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
     </>
   );
 }
