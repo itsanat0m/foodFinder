6a416efa7c0b9ca79edcc013c38f84c0ff8b545a
Julian Vitenson <jviten@ad.unc.edu>
2025-10-11 19:03:57 -0400

    More Css and Home page changes

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -4,14 +4,20 @@ import './HomePage.css';
 const HomePage = () => {
   return (
     <>
-      <div id="header1">
+      <div>
         <header>
-          <h1>Welcome to Food Finder</h1>
+          <div className="roundedBox header1">
+            <h1>Welcome to Food Finder</h1>
+          </div>
         </header>
         <main>
-          <img className="default-ram" src="assets/ramChef.png" alt="Chef Rami" >
-          </img>
-          <p>Don't know where to eat? Chef Rami has got you covered</p>
+          <div className="ramContainer">
+            <div className="speech-bubble">
+              <p>Don't know where to eat? Chef Rami has got you covered</p>
+            </div>
+            <img className="default-ram" src="assets/ramChef.png" alt="Chef Rami" >
+            </img>
+          </div>
         </main>
         <button>
           Click to Start!
