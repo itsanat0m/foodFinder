5994c8ea27c26b0a1662dc9bc541aea66542cce7
ebankson <eliasbankson@dhcp-152-23-57-11.wireless-1x.unc.edu>
2025-10-11 14:08:10 -0400

    added button to homepage

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -10,14 +10,12 @@ const HomePage = () => {
             <main>
                 <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
             </main>
-        </div>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>cd
+             <button>
+                Nomral Button
+             </button>
+             <p>
+                Edit <code>src/App.jsx</code> and save to test HMR
+             </p>cd
         </div>
     </>
   );
