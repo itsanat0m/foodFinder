6df48cbde1f4b37f25f92b7b126b2b961e9edf0a
vinzy <vinzy@unc.edu>
2025-10-11 13:49:17 -0400

    add hompage component

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,28 @@
+
+
+
+
+const HomePage = () => {
+  return (
+    <>
+        <div>
+            <header>
+                <h1>My Simple React Home Page</h1>
+            </header>
+            <main>
+                <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
+            </main>
+        </div>
+        <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>cd
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
