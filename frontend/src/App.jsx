7bc01240d85120fb1ae045ad010aeb250021d7be
vinzy <vinzy@unc.edu>
2025-10-11 15:11:27 -0400

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Homepage from './components/HomePage'
+import HomePage from './components/HomePage';
 import './App.css'
-import QuestionBox from './components/QuestionBox';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <Homepage />
-      </div>
+      <HomePage/>
     </>
   )
 }
